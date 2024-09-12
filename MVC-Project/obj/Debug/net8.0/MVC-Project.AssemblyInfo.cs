085c5a5fb3daf568dc275093558ebc4bf2114d46
m-anass <manas8269@gmail.com>
2024-09-12 10:20:46 +0300

    c

--- MVC-Project/obj/Debug/net8.0/MVC-Project.AssemblyInfo.cs
+++ MVC-Project/obj/Debug/net8.0/MVC-Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MVC-Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a3ffd15a44cfe3babf8ea0c4570cb13c37a7df4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b771d6c8ae44f9b3e4bcdb64410c8627cc73756")]
 [assembly: System.Reflection.AssemblyProductAttribute("MVC-Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MVC-Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
